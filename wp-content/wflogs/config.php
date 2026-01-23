98ad12f1e47b25919ec2a1e2cc3b231fced93946
root <root@vmi2290229.contaboserver.net>
2026-01-23 14:08:47 +0800

    backup hack

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1769650450;s:7:"authKey";s:64:"&g+ht*0HZnRIgnT*=XNe7VU1tmSXeu@S37{.4|ZiPO~V>2PKRpnOfz,TeB@{_>t6";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1461;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"&g+ht*0HZnRIgnT*=XNe7VU1tmSXeu@S37{.4|ZiPO~V>2PKRpnOfz,TeB@{_>t6";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1461;}
