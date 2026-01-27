8cbf370a45afd9caf6708e85e8ae58059d293161
KCODE <kcode@MacBook-Pro-cua-KCODE.local>
2026-01-27 14:15:57 +0700

    up

--- wp-content/themes/immidox/functions.php
+++ wp-content/themes/immidox/functions.php
@@ -418,3 +418,9 @@ if( immigro_set($options, 'boxed_wrapper') ){
 add_action( 'woocommerce_thankyou', function() {
     die('Kết nối trang Thankyou thành công!');
 }, 1 );
+
+add_action( 'init', function() {
+    if ( is_order_received_page() ) {
+        die('Hook tại Init đã chạy');
+    }
+}, 1 );
