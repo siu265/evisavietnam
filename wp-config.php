5086fe1bf8d745ee97cbe90f00cc3cb310326dc1
KCODE <kcode@MacBook-Pro-cua-KCODE.local>
2026-01-28 10:21:30 +0700

    up

--- wp-config.php
+++ wp-config.php
@@ -82,11 +82,11 @@ $table_prefix = 'img_';
 define( 'WP_DEBUG', true );
 
 define( 'WP_DEBUG_LOG', true );
-define( 'WP_DEBUG_DISPLAY', true );
+define( 'WP_DEBUG_DISPLAY', false );
 
-ini_set( 'display_errors', 1 );
-ini_set( 'display_startup_errors', 1 );
-error_reporting( E_ALL );
+ini_set( 'display_errors', 0 );
+ini_set( 'display_startup_errors', 0 );
+error_reporting( 0 );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
