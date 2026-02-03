9fb251245da5063d69cf4185ccce993d308d3d4a
KCODE <kcode@MacBook-Pro-cua-KCODE.local>
2026-02-03 12:58:26 +0700

    up

--- wp-content/plugins/immigro-plugin/elementor/team_block.php
+++ wp-content/plugins/immigro-plugin/elementor/team_block.php
@@ -175,7 +175,26 @@ class Team_Block extends Widget_Base {
 				'placeholder' => __( 'Enter your Sub title', 'rashid' ),
 			]
 		);
-		
+
+		$this->add_control(
+			'extra_images',
+			[
+				'label'   => esc_html__( 'Extra Images', 'immigro' ),
+				'type'    => Controls_Manager::GALLERY,
+				'default' => [],
+			]
+		);
+
+		$this->add_control(
+			'show_social',
+			[
+				'label'   => esc_html__( 'Show Social Icons', 'immigro' ),
+				'type'    => Controls_Manager::SWITCHER,
+				'default' => 'yes',
+				'label_on'  => esc_html__( 'Yes', 'immigro' ),
+				'label_off' => esc_html__( 'No', 'immigro' ),
+			]
+		);
 
 		$this->add_control(
 			'text',
@@ -213,6 +232,13 @@ class Team_Block extends Widget_Base {
 					],
 				'fields' => 
 					[	
+							'block_title' =>
+							[
+								'name' => 'block_title',
+								'label' => esc_html__( 'Label', 'immigro' ),
+								'type'  => Controls_Manager::TEXT,
+								'placeholder' => esc_html__( 'Social', 'immigro' ),
+							],
 
 							  'block_icons'=>	
 
@@ -244,7 +270,139 @@ class Team_Block extends Widget_Base {
 	);
 		
 		
-$this->end_controls_section();	
+$this->end_controls_section();
+
+		// Style: Extra Images
+		$this->start_controls_section(
+			'extra_images_style',
+			[
+				'label' => esc_html__( 'Extra Images Style', 'immigro' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+		$this->add_control(
+			'extra_img_size',
+			[
+				'label'   => esc_html__( 'Size (px)', 'immigro' ),
+				'type'    => Controls_Manager::NUMBER,
+				'default' => 50,
+				'min'     => 24,
+				'max'     => 120,
+				'selectors' => [
+					'{{WRAPPER}} .team-extra-images li' => 'width: {{VALUE}}px; height: {{VALUE}}px;',
+					'{{WRAPPER}} .team-extra-images li img' => 'width: {{VALUE}}px; height: {{VALUE}}px; object-fit: cover;',
+				],
+			]
+		);
+		$this->add_control(
+			'extra_img_gap',
+			[
+				'label'   => esc_html__( 'Gap (px)', 'immigro' ),
+				'type'    => Controls_Manager::NUMBER,
+				'default' => 8,
+				'min'     => 0,
+				'selectors' => [
+					'{{WRAPPER}} .team-extra-images li' => 'margin: 0 {{VALUE}}px {{VALUE}}px 0;',
+				],
+			]
+		);
+		$this->add_control(
+			'extra_img_radius',
+			[
+				'label'      => esc_html__( 'Border Radius (%)', 'immigro' ),
+				'type'       => Controls_Manager::SLIDER,
+				'size_units' => [ '%' ],
+				'range'      => [
+					'%' => [ 'min' => 0, 'max' => 50 ],
+				],
+				'default'    => [ 'unit' => '%', 'size' => 50 ],
+				'selectors'  => [
+					'{{WRAPPER}} .team-extra-images li, {{WRAPPER}} .team-extra-images li img' => 'border-radius: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+		$this->end_controls_section();
+
+		// Style: Title
+		$this->start_controls_section(
+			'title_style',
+			[
+				'label' => esc_html__( 'Title Style', 'immigro' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			[
+				'name'     => 'title_typography',
+				'selector' => '{{WRAPPER}} .inner-box h3, {{WRAPPER}} .lower-content h3',
+			]
+		);
+		$this->add_control(
+			'title_color',
+			[
+				'label'     => esc_html__( 'Color', 'immigro' ),
+				'type'      => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .inner-box h3 a, {{WRAPPER}} .lower-content h3 a' => 'color: {{VALUE}};',
+				],
+			]
+		);
+		$this->end_controls_section();
+
+		// Style: Subtitle
+		$this->start_controls_section(
+			'subtitle_style',
+			[
+				'label' => esc_html__( 'Subtitle Style', 'immigro' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			[
+				'name'     => 'subtitle_typography',
+				'selector' => '{{WRAPPER}} .designation',
+			]
+		);
+		$this->add_control(
+			'subtitle_color',
+			[
+				'label'     => esc_html__( 'Color', 'immigro' ),
+				'type'      => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .designation' => 'color: {{VALUE}};',
+				],
+			]
+		);
+		$this->end_controls_section();
+
+		// Style: Description
+		$this->start_controls_section(
+			'text_style',
+			[
+				'label' => esc_html__( 'Description Style', 'immigro' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			[
+				'name'     => 'text_typography',
+				'selector' => '{{WRAPPER}} .inner-box p, {{WRAPPER}} .lower-content p',
+			]
+		);
+		$this->add_control(
+			'text_color',
+			[
+				'label'     => esc_html__( 'Color', 'immigro' ),
+				'type'      => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .inner-box p, {{WRAPPER}} .lower-content p' => 'color: {{VALUE}};',
+				],
+			]
+		);
+		$this->end_controls_section();	
 				
 		
 		}
@@ -259,10 +417,9 @@ $this->end_controls_section();
 	protected function render() {
 		$settings = $this->get_settings_for_display();
 		$allowed_tags = wp_kses_allowed_html('post');
+		$extra_imgs  = isset( $settings['extra_images'] ) && is_array( $settings['extra_images'] ) ? $settings['extra_images'] : [];
 		?>
-
-
-		
+		<style>.team-extra-images{list-style:none;margin:10px 0 15px 0;padding:0;display:flex;flex-wrap:wrap;align-items:center}.team-extra-images li{display:inline-block;overflow:hidden;box-shadow:0 10px 50px rgba(34,34,34,0.2);background:#fff}.team-extra-images li img{display:block;vertical-align:top}</style>
 		<?php  if ( 'style1' === $settings['style'] ) : ?>
 
 		<section class="team-section centred <?php echo esc_attr($settings['sec_class']);?>">
@@ -282,12 +439,25 @@ $this->end_controls_section();
                                 <div class="content-box">
                                     <h3><a href="<?php echo esc_url($settings['btnlink']['url']);?>"><?php echo $settings['title'];?></a></h3>
                                     <span class="designation"><?php echo $settings['subtitle'];?></span>
+                                    <?php if ( ! empty( $extra_imgs ) ) :
+                                    ?>
+                                    <ul class="team-extra-images clearfix">
+                                        <?php foreach ( $extra_imgs as $img ) :
+                                            $url = ! empty( $img['url'] ) ? $img['url'] : ( ! empty( $img['id'] ) ? wp_get_attachment_url( (int) $img['id'] ) : '' );
+                                            if ( $url ) :
+                                        ?>
+                                        <li><img src="<?php echo esc_url( $url ); ?>" alt="" /></li>
+                                        <?php endif; endforeach; ?>
+                                    </ul>
+                                    <?php endif; ?>
                                     <p><?php echo $settings['text'];?></p>
+                                    <?php if ( ( $settings['show_social'] ?? 'yes' ) === 'yes' ) : ?>
                                     <ul class="social-links clearfix">
 									<?php foreach($settings['repeat'] as $item):?>	
-                                        <li><a href="<?php echo esc_url($item['block_btnlink']['url']);?>"><i class="<?php echo str_replace("icon ", " ", esc_attr( $item['block_icons']['value']));?>"></i></a></li>
+                                        <li><a href="<?php echo esc_url($item['block_btnlink']['url']);?>"><i class="<?php echo str_replace("icon ", " ", esc_attr( $item['block_icons']['value'] ) );?>"></i></a></li>
 									<?php endforeach; ?>
                                     </ul>
+                                    <?php endif; ?>
                                 </div>
                             </div>
                         </div>
@@ -315,6 +485,7 @@ $this->end_controls_section();
 									<?php endif;?>
 								</figure>
                                 <div class="lower-content p_relative">
+                                    <?php if ( ( $settings['show_social'] ?? 'yes' ) === 'yes' ) : ?>
                                     <div class="share-box p_absolute centred">
                                         <a href="#" class="share-icon fs_14 b_radius_50 d_iblock"><i class="fas fa-share-alt"></i></a>
                                         <ul class="share-links p_absolute clearfix">
@@ -325,8 +496,19 @@ $this->end_controls_section();
 										<?php endforeach; ?>
                                         </ul>
                                     </div>
+                                    <?php endif; ?>
 									<h3><a href="<?php echo esc_url($settings['btnlink']['url']);?>"><?php echo $settings['title'];?></a></h3>
                                     <span class="designation"><?php echo $settings['subtitle'];?></span>
+                                    <?php if ( ! empty( $extra_imgs ) ) : ?>
+                                    <ul class="team-extra-images clearfix">
+                                        <?php foreach ( $extra_imgs as $img ) :
+                                            $url = ! empty( $img['url'] ) ? $img['url'] : ( ! empty( $img['id'] ) ? wp_get_attachment_url( (int) $img['id'] ) : '' );
+                                            if ( $url ) :
+                                        ?>
+                                        <li><img src="<?php echo esc_url( $url ); ?>" alt="" /></li>
+                                        <?php endif; endforeach; ?>
+                                    </ul>
+                                    <?php endif; ?>
                                     <p><?php echo $settings['text'];?></p>
                                 </div>
                             </div>
@@ -355,6 +537,7 @@ $this->end_controls_section();
 										<div class="noimage"></div>
 										<?php endif;?>
 									</figure>
+                                    <?php if ( ( $settings['show_social'] ?? 'yes' ) === 'yes' ) : ?>
                                     <ul class="social-links clearfix">
 										<?php foreach($settings['repeat'] as $item):?>	
                                             <li>
@@ -362,10 +545,21 @@ $this->end_controls_section();
                                             </li>
 										<?php endforeach; ?>
                                     </ul>
+                                    <?php endif; ?>
                                 </div>
                                 <div class="lower-content">
                                     <h3><a href="<?php echo esc_url($settings['btnlink']['url']);?>"><?php echo $settings['title'];?></a></h3>
                                     <span class="designation"><?php echo $settings['subtitle'];?></span>
+                                    <?php if ( ! empty( $extra_imgs ) ) : ?>
+                                    <ul class="team-extra-images clearfix">
+                                        <?php foreach ( $extra_imgs as $img ) :
+                                            $url = ! empty( $img['url'] ) ? $img['url'] : ( ! empty( $img['id'] ) ? wp_get_attachment_url( (int) $img['id'] ) : '' );
+                                            if ( $url ) :
+                                        ?>
+                                        <li><img src="<?php echo esc_url( $url ); ?>" alt="" /></li>
+                                        <?php endif; endforeach; ?>
+                                    </ul>
+                                    <?php endif; ?>
                                 </div>
                             </div>
                         </div>
